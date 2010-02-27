7d327e7493c04c3e668a2191f080bf99fbe77e76
Lawrence Sebald <ljsebald@users.sourceforge.net>
2010-02-27 16:28:19 +0000

    Update copyright on lots of changed files...

--- src/lobby.h
+++ src/lobby.h
@@ -1,6 +1,6 @@
 /*
     Sylverant Ship Server
-    Copyright (C) 2009 Lawrence Sebald
+    Copyright (C) 2009, 2010 Lawrence Sebald
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 3 as
